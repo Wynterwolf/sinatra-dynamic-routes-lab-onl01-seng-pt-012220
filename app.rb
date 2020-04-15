3477e31a78edb78dfa66af2bd7e84c69428fc02f
Samantha Jordan <shinyguns@gmail.com>
2020-04-15 21:15:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  #reverse_name
+  get '/reversename/:name' do
+    @reversename = params[:name].reverse
+    @reversename
+  end
 
 end
