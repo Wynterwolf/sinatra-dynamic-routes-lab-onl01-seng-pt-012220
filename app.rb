ce2e4d7d4851107137ad6d33d4bf92f49268c167
Samantha Jordan <shinyguns@gmail.com>
2020-04-15 21:20:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,4 +9,19 @@ class App < Sinatra::Base
     @reversename
   end
 
+  #square
+  get '/square/:number' do
+    @square = params[:number].to_i ** 2 
+    @square.to_s 
+  end
+  
+  #return single string with two local_variables
+  get '/say/:number/:phrase' do
+    string = ""
+    @number = params[:number].to_i 
+    @phrase = params[:phrase]
+    @number.times {string += "#{@phrase}\n" }
+    string 
+  end
+  
 end
