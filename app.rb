a02d4eb0fa21bff583b43839ba2a8268b7f1f4c2
Samantha Jordan <shinyguns@gmail.com>
2020-04-15 21:29:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,5 +23,15 @@ class App < Sinatra::Base
     @number.times {string += "#{@phrase}\n" }
     string 
   end
-  
+   
+   #many word string
+   get '/say/:word1/:word2/:word3/:word4/:word5' do
+     @one = params[:word1]
+     @two = params[:word2]
+     @three = params[:word3]
+     @four = params[:word4]
+     @five = params[:word5]
+     "#{one} #{two} #{three} #{four} #{five}."
+   end
+   
 end
